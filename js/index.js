4970f12db67b6cad370376008508c3fa873e0107
Mika88 <mikaspivak@gmail.com>
2019-04-05 19:56:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function getRepositories() {
   let input = document.getElementById("username").value
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/input/repos');
+  req.open('GET', `https://api.github.com/users/${input}/repos`);
   req.send();
 }
 
