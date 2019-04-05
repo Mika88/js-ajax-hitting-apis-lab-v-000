f6c0a5a648fbdb1727f3cf46d2a3eb1c104bf762
Mika88 <mikaspivak@gmail.com>
2019-04-05 19:54:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,10 +7,11 @@ function displayRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-function getRepositories(username) {
+function getRepositories() {
+  let input = document.getElementById("username").value
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/username/repos');
+  req.open('GET', 'https://api.github.com/users/input/repos');
   req.send();
 }
 
