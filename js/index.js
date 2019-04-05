e219b0e1d44f70ed86fed159eb0b2c2531ef80c5
Mika88 <mikaspivak@gmail.com>
2019-04-05 22:53:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,7 +22,6 @@ function getCommits(el) {
   let username = document.getElementById("username").value
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  console.log('https://api.github.com/repos/' + username + '/' + name + '/commits')
   req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/' + username + '/' + name + '/commits');
   req.send();
