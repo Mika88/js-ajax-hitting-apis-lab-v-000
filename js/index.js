54bd326a75e82aac6baa56f11351fc42856e2d41
Mika88 <mikaspivak@gmail.com>
2019-04-05 22:22:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos.map(r =>
-    '<li><a href="' + r.url + '">' +
+    '<li><a href="' + r.html_url + '">' +
        r.name + '</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>'
     )
     .join('')}</ul>`;
