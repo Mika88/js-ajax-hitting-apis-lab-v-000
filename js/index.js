319c93c53c8ff8c62512cd530b39bd4410a09c0f
Mika88 <mikaspivak@gmail.com>
2019-04-05 19:37:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,12 +2,7 @@
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos
-    .map(r =>
-      '<li>' +
-       r.name +
-       ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>'
-    )
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name +' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>')
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
@@ -15,7 +10,7 @@ function displayRepositories() {
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.open('GET', 'https://api.github.com/users/:username/repos');
   req.send();
 }
 
