7966b35b7fed2018c6c718ea8d568d32396b76ab
Mika88 <mikaspivak@gmail.com>
2019-04-05 19:37:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function getRepositories() {
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
+  req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
